1bb6491adef351be1c4037fb3e15ea7034cdeadd
czqzju <czqzju@gmail.com>
2019-01-22 09:24:10 +0800

    complete Castle on the grid

--- src/Castle_on_the_Grid.java
+++ src/Castle_on_the_Grid.java
@@ -20,11 +20,14 @@ public class Castle_on_the_Grid {
 	static int n;
     
 	static int minimumMoves(String [] grid, int startX, int startY, int goalX, int goalY) {
+		
+		n = grid.length;
 		game = new int[n][n];
 		visited = new int[n][n];
 		
 		
-		for(int i = 0; i < n; i++)
+		
+		for(int i = 0; i < n; i++) {
 			for(int j = 0; j < n; j++) {
 				if(grid[i].charAt(j) == '.') {
 					game[i][j] = 100;
@@ -33,7 +36,52 @@ public class Castle_on_the_Grid {
 					game[i][j] = -1;
 				}
 			}
-		return;
+		}
+		Point tmp = new Point(startX, startY);
+		game[startX][startY] = 0;
+		move.add(tmp);
+		
+		while(!move.isEmpty()) {
+			Point cur = move.remove();
+			
+			if(visited[cur.x][cur.y] == 0) {
+				visited[cur.x][cur.y] = 1;
+				moveNext(cur);
+			}
+		}
+		
+		return game[goalX][goalY];
+	}
+	
+	static void moveNext(Point cur) {
+		int x = cur.x;
+		int y = cur.y;
+		int value = game[cur.x][cur.y];
+		
+		for(int i = x + 1; i < n && game[i][y] != -1; i++) {
+			updateStep(i, y, value);
+			move.add(new Point(i, y));
+		}
+		
+		for(int i = x - 1; i >= 0 && game[i][y] != -1; i--) {
+			updateStep(i, y, value);
+			move.add(new Point(i, y));
+		}
+		for(int j = y + 1; j < n && game[x][j] != -1; j++) {
+			updateStep(x, j, value);
+			move.add(new Point(x, j));
+		}
+		
+		for(int j = y - 1; j >= 0 && game[x][j] != -1; j--) {
+			updateStep(x, j, value);
+			move.add(new Point(x, j));
+		}
+	}
+	
+	static void updateStep(int x, int y, int value) {
+		if(game[x][y] > value + 1) {
+			game[x][y] = value + 1;
+		}
 	}
 
     public static void main(String[] args) {
