87bf701516714ef1472477bf20cc7352cd34f308
czqzju <czqzju@gmail.com>
2019-02-26 14:00:26 +0800

    Update Really_Special_Subtree_Kruskal.java

--- src/Graphs/Really_Special_Subtree_Kruskal.java
+++ src/Graphs/Really_Special_Subtree_Kruskal.java
@@ -1,5 +1,5 @@
 package Graphs;
-//https://www.hackerrank.com/challenges/kruskalmstrsub/problem
+
 import java.io.*;
 import java.math.*;
 import java.security.*;
@@ -11,6 +11,7 @@ import java.util.regex.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
+
 
 class Result {
 
@@ -29,14 +30,52 @@ class Result {
      * 3. An edge exists between <name>From[i] and <name>To[i]. The weight of the edge is <name>Weight[i].
      *
      */
+	private static int findParent(int node, int[] parent) {
+		int start = node;
+		while(parent[start] != start) start = parent[start];
+		return start;
+	}
 
     public static int kruskals(int gNodes, List<Integer> gFrom, List<Integer> gTo, List<Integer> gWeight) {
-    	return 10;
+    	int n = gFrom.size();
+    	int[][] edges = new int[n][3];
+    	int[] parent = new int[gNodes + 1];
+    	int numEdges = 0;
+    	for(int i = 0; i < n; i ++) {
+    		edges[i][0] =  gFrom.get(i);
+    		edges[i][1] = gTo.get(i);
+    		edges[i][2] = gWeight.get(i);
+    	}
+    	Arrays.sort(edges, new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if(o1[2] > o2[2]) return 1;
+				else if(o1[2] < o2[2]) return -1;
+				else return 0;
+			}
+		});
+    	
+    	for(int i = 0; i < gNodes + 1; i++) parent[i] = i;
+    	
+    	int res = 0;
+    	for(int i = 0; i < n; i++) {
+    		int parent0 = findParent(edges[i][0], parent);
+    		int parent1 = findParent(edges[i][1], parent);
+    		if(parent0 != parent1) {
+    			parent[parent1] = parent0;
+    			res += edges[i][2];
+    			numEdges++;
+    			if(numEdges == gNodes - 1) break;
+    		}
+    	}
+    	return res;
+    	
     }
 
 }
 
-public class Really_Special_Subtree_Kruskal {
+public class Really_Special_Subtree_kruskal {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 //        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
@@ -64,7 +103,7 @@ public class Really_Special_Subtree_Kruskal {
 
         int res = Result.kruskals(gNodes, gFrom, gTo, gWeight);
 
-        // Write your code here.
+        System.out.println(res);
 
         bufferedReader.close();
 //        bufferedWriter.close();
